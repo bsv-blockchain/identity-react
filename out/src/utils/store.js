dee3b4b1a6da85bae2dc4434ac02a3d58554ce02
Brayden Langley <brayden167@gmail.com>
2024-02-20 15:00:54 -0800

    Build

--- out/src/utils/store.js
+++ out/src/utils/store.js
@@ -3,23 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.useStore = void 0;
 const zustand_1 = require("zustand");
 const sdk_ts_1 = require("@babbage/sdk-ts");
-function debounce(func, waitFor) {
-    let timeoutId = null;
-    return function (...args) {
-        if (timeoutId !== null) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(() => func(...args), waitFor);
-    };
-}
 const isIdentityKey = (key) => {
     const regex = /^(02|03|04)[0-9a-fA-F]{64}$/;
     return regex.test(key);
 };
 exports.useStore = (0, zustand_1.create)((set) => ({
     identities: [],
-    fetchIdentities: debounce(async (query, setIsLoading) => {
-        console.log('is it fetching...?');
+    fetchIdentities: async (query, setIsLoading) => {
         setIsLoading(true);
         let results;
         // Figure out if the query is by IdentityKey
@@ -36,16 +26,6 @@ exports.useStore = (0, zustand_1.create)((set) => ({
                 },
                 description: 'Discover MetaNet Identity'
             });
-            // TODO: Create better solution!
-            // Quick Hack to check discord handles
-            if (results.length === 0) {
-                results = await (0, sdk_ts_1.discoverByAttributes)({
-                    attributes: {
-                        userName: query
-                    },
-                    description: 'Discover MetaNet Identity'
-                });
-            }
         }
         const matchingIdentities = results.map((x) => {
             // Test adding varied name props
@@ -63,9 +43,8 @@ exports.useStore = (0, zustand_1.create)((set) => ({
                 certifier: x.certifier
             };
         });
-        console.log(matchingIdentities);
         setIsLoading(false);
         set({ identities: matchingIdentities });
-    }, 500),
+    },
 }));
 //# sourceMappingURL=store.js.map
