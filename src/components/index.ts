15b79a79d49c7df0a7877422a09fe9f689370562
Dan Feinstein <otherdaniel@gmail.com>
2024-04-19 09:20:47 -0700

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as IdentitySearchFieldCUSTOM } from "./IdentitySearchField"
-export { default as IdentitySearchField } from "./IdentitySearchFieldCUSTOM"
+export { default as IdentitySearchFieldCUSTOM } from "./IdentitySearchFieldCUSTOM"
+export { default as IdentitySearchField } from "./IdentitySearchField"
 export { default as IdentityCard } from "./IdentityCard"
 export { default as IdentitySearchForm } from "./IdentitySearchForm"
