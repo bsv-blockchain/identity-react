6b5cad170b296037301143d8e2f075e0d50665a7
Dan Feinstein <otherdaniel@gmail.com>
2024-04-19 09:15:23 -0700

    Revert to original search component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as IdentitySearchFieldOLD } from "./IdentitySearchFieldOLD"
-export { default as IdentitySearchField } from "./IdentitySearchField"
+export { default as IdentitySearchFieldCUSTOM } from "./IdentitySearchField"
+export { default as IdentitySearchField } from "./IdentitySearchFieldCUSTOM"
 export { default as IdentityCard } from "./IdentityCard"
 export { default as IdentitySearchForm } from "./IdentitySearchForm"
